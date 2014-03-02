1d4b740a2a3e10c406cde80237908b985d5b7014
daicham <daicham@gmail.com>
2014-03-02 15:59:44 +0900

    added commands in ng-repeat
    $index, $first, $last, $middle
    ng-class-even, ng-class-mod

--- myscript.js
+++ myscript.js
@@ -3,6 +3,9 @@ var mainCtrl = function($scope) {
     {"name": "taguchi", "score": 55.22},
     {"name": "tanaka", "score": 38.22},
     {"name": "yamada", "score": 11.22},
+    {"name": "suzuki", "score": 90.22},
+    {"name": "sasaki", "score": 98.22},
+    {"name": "watanabe", "score": 4.22}
   ];
   $scope.today = new Date();
 };
